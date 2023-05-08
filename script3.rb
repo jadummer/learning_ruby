9ba02d9c9c576521a95e6f9a5c9059f591ab90d2
jadummer <50461418+jadummer@users.noreply.github.com>
2023-05-08 14:15:16 -0400

    Update script3.rb

--- script3.rb
+++ script3.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
 
+# Take JSON file path from the command line and verify the file exists, 
+# print a message to the screen saying so; 
+# add a message to print when the file does not.
+
 if File.exist?("manifest.json")
-	puts "manifest.json"
+	puts "manifest.json exists"
 else
 	puts "There is no json file in this directory."
 end
