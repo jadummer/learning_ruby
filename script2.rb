ed4952244b40e448a1ffcc25087a6dbe4b029bdf
jadummer <50461418+jadummer@users.noreply.github.com>
2023-05-08 14:13:15 -0400

    Update script2.rb

--- script2.rb
+++ script2.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 # print the arguments as a comma-separated list (Array#join)
+# ruby script2.rb hello how are you
+# returns ["hello," "how," "are," "you"]
 
 input_array = ARGV
 
