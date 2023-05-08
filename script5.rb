e8a57c24ad045037f102cc2bc048d12e7c8363f2
jadummer <50461418+jadummer@users.noreply.github.com>
2023-05-08 14:17:33 -0400

    Update script5.rb

--- script5.rb
+++ script5.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 require 'json'
 
+# Open one JSON file and load it using the JSON library and print the top level keys
+
 json = File.read("manifest.json")
 data_hash = JSON.parse(json)
 puts data_hash
