3caed136b31e29e6538919aaa1de754c1f4069e5
Jessica Dummer <jadummer@gmail.com>
2023-05-08 16:01:35 -0400

    finished script 6

--- script6.rb
+++ script6.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/env ruby
 require 'json'
+require 'pp'
 
-# Open one JSON file and load it using the JSON library and print the top level keys
+# Open one JSON file and print each image @id, geting each ID using Hash bracket [] notation
 
 json = File.read("manifest.json")
-data_hash = JSON.parse(json)
-puts data_hash.keys
+data_hash = JSON.parse(json) # is a Hash
+sequences = data_hash["sequences"] # is an Array
+canvases = sequences[0]["canvases"] # is an Array
+canvases.each do |canvas| 
+	#for each hash get the first value of the @id key
+	puts canvas["@id"]
+end
+
