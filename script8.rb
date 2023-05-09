a0a034cf251d698473f7671610ee574476598af6
Jessica Dummer <jadummer@gmail.com>
2023-05-09 11:58:01 -0400

    updating script 8

--- script8.rb
+++ script8.rb
@@ -3,7 +3,4 @@
 # Script 8: Open your ARK list and print each line to the screen, using the File.open() do # |f| ... end block.
 
 f = File.open("arkids.txt")
-f.each do |line|
-	puts line
-end
-f.close()
+f.each { |line| puts line }
