396b85e9355db7eda913157ff510c3ec93f83b71
Jessica Dummer <jadummer@gmail.com>
2023-05-10 15:25:25 -0400

    updated script 4 to accept any path/file as an arguement

--- script4.rb
+++ script4.rb
@@ -1,9 +1,16 @@
 #!/usr/bin/env ruby
 # require 'json'
 
- # Open one JSON file and print each line to the screen, using
+# Open one JSON file and print each line to the screen, using
 # File.open do |f|
 # end
 
-File.open("data/iiifmanifests.txt").each {|line| puts line}
+# CHANGE: If time permits, change this script to accept an argument 
+# so it will work with any text file.
+
+path = ARGV
+path = path.join("")
+puts path
+
+File.open(path).each {|line| puts line}
 
