41288d442bf1f905080794b360410da7192c3b4b
Jessica Dummer <jadummer@gmail.com>
2023-05-10 16:30:58 -0400

    updating script7 to use the hash.dig method correctly

--- script7.rb
+++ script7.rb
@@ -2,13 +2,23 @@
 require 'json'
 require 'pp'
 
-# Open one JSON file and print each image @id using, getting each @id using Hash#dig
+=begin
 
-json = File.read("data/manifest.json")
-data_hash = JSON.parse(json) # is a Hash
-sequences = data_hash.dig("sequences")
-canvases = sequences[0].dig("canvases")
-canvases.each do |canvas| 
-	#for each hash get the first value of the @id key
-	puts canvas["@id"]
+1. Open one JSON file and print each image @id using, getting each @id using Hash#dig
+
+2. OOPS: My bad, the manifest was a bad choice here. It doesn't really exercise the 
+Hash#dig method. So, do this, open and load data/dir2/ds-catalog-dump-20230328.json 
+and use Hash#dig to get the value for each top-level hash in the list 
+(thing['labels']['en']['value']). Note that they all have labels.
+
+=end
+
+path = ARGV
+path = path.join("")
+
+json = File.read(path)
+data_hash = JSON.parse(json) # is an Array
+data_hash.each do |h|
+	puts h.dig('labels', 'en', 'value')
 end
+
